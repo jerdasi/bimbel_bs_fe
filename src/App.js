1fa98fb7459b40c74c9fae350761e8bf38b8e704
julsyeah <181113723@students.mikroskil.ac.id>
2022-06-30 22:12:21 +0700

    style : add Pembayaran Berhasil and Routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Pages/LandingPage/LandingPage';
+import PembayaranBerhasil from './Pages/Pembayaran/PembayaranBerhasil';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Navbar/>
-      <LandingPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}></Route>
+          <Route path='/pembayaran-berhasil' element={<PembayaranBerhasil/>}></Route>
+        </Routes>
+      </BrowserRouter>
+        
       <Footer/>
     </>
   );
---
+++ src/Pages/Pembayaran/PembayaranBerhasil.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import image from '../../Images/pembayaran_berhasil.svg'
+
+function PembayaranBerhasil() {
+  return (
+    <>
+        
+        <img src={image}/>
+        <h1>Selamat Pembayaran Kamu Telah Berhasil</h1>
+        <h3>Terimakasih Telah Bergabung di Bimbingan Belajar Beta Smart</h3>
+        
+    </>
+  )
+}
+
+export default PembayaranBerhasil
