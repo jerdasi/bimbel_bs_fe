5e72ee89e8d2132d24e09fe01509b465194c2511
julsyeah <181113723@students.mikroskil.ac.id>
2022-07-15 01:35:43 +0700

    style: pembayaran done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,14 +7,14 @@ import {TbLocation} from 'react-icons/tb'
 function Footer() {
     
   return (
-    <div className='w-full bg-white px-16'>
-        <div className='w-full flex justify-between py-4 h-24'>
-            <img src={logo}/>
+    <div className='w-full bg-white px-16 py-4'>
+        <div className='w-full md:flex justify-between py-4 h-24'>
+            <img src={logo} className='hidden md:flex'/>
             <div className=''>
                 <p className='my-0 flex flex-row'>Ikuti <span className='text-merah-bs mx-1' > Beta Smart </span> di media sosial<BsFacebook size={20} className='mx-1'/><BsInstagram size={20}  className='mx-1'/></p>
                 <p className='my-0 flex flex-row'>Hubungi <span className='text-merah-bs mx-1'> Beta Smart </span> di <MdMarkEmailUnread size={20} className='mx-1'/><BsWhatsapp size={20} className='mx-1'/> </p>
             </div>
-            <button className='flex p-2 justify-center items-center text-white font-bold bg-merah-bs rounded-md'><TbLocation size={20} className='mx-2'/>Lihat Alamat di GMaps</button>
+            <button className='flex mt-1 p-2 justify-center items-center text-white font-bold bg-merah-bs rounded-md'><TbLocation size={20} className='mx-2'/>Lihat Alamat di GMaps</button>
         </div>
     </div>
   )
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import hero from '../Assets/Images/hero.png'
 import whyus from '../Assets/Images/whyus.png'
 import insignia from '../Assets/Images/insignia.png'
+import book from '../Assets/Images/book.svg'
 import whyusjson from '../Components/json/whyus.json'
 import testimoni from '../Components/json/testimoni.json'
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
@@ -35,15 +36,19 @@ function LandingPage() {
         </div>
 
       </div>
-      <div className='px-10 py-16 mx-auto' >
+      <div className='px-10 py-16 mx-auto bg-antique' >
         <h1 className='font-bold text-3xl'>Daftar dan Mulai Belajar</h1>
         <p className='pb-2'>Pilih sendiri atau ikuti rekomendasi kami</p>
         <div className=''>
           <ul className='flex flex-row justify-start overflow-x-auto'>
+            <li className='w-1/4 p-2 md:p-4 md:w-[200px] text-center mx-auto md:mx-0 border-2 border-gray-900 rounded-md items-center cursor-pointer hover:scale-105 ease-in-out duration-300 justify-center'>
+              <img src={insignia} className='w-[90px]'/>
+              <h4 className='text-sm'>Rekomendasi</h4>
+            </li>
             {jenjang.map((item) => {
               return (
                 <li key={item.id} className='w-1/4 p-2 md:p-4 md:w-[200px] text-center mx-auto md:mx-0 border-2 border-gray-900 rounded-md items-center cursor-pointer hover:scale-105 ease-in-out duration-300 justify-center'>
-                  <img src={insignia} className='w-[90px]' />
+                  <img src={book} className='w-[90px]' />
                   <h4 className='text-sm'>{item.nama_jenjang}</h4>
                 </li>
               )
@@ -78,14 +83,14 @@ function LandingPage() {
         </div>
       </div>
 
-      <div className='px-10 py-16 mx-auto'>
+      <div className='px-10 py-16 mx-auto bg-antique'>
         <h1>Kisah Sukses </h1>
         <p>Bergabung dengan Beta Smart bukan pilihan yang salah. Ikuti jejak mereka!</p>
         <div>
           {testimoni.map((item, index) => {
             return (
               <div className='lg:w-1/2 bg-white p-4 my-2 items-center text-center justify-center rounded-xl'>
-                <img src={item.image} />
+                <img src={item.image} className='mx-auto' />
                 <h1 className='py-4'>{item.nama} - {item.paket}</h1>
                 <p className='text-left'>{item.ulasan}</p>
               </div>
